e438799fbd2bd88919f755645666bb1a568a77d0
harenarium <carolinel2610@gmail.com>
2017-11-09 03:59:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = "Tammy";
+
+function upperCaseCustomerName(){
+  customerName.toUpperCase;
+}
+
+function setBestCustomer(){
+  var bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+  bestCustomer = 'me!'
+}
+
+const leastFavoriteCustomer = 'bob'
+
+fuction changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'still bob'
+}
+
+function attemptTwoFavoriteCustomers(){
+  let favoriteCustomer = 'me'
+  let favoriteCustomer = 'me again'
+}
